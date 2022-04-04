481ed2721ad35a3100a91e7bbc2bf4568de2e02f
RudNickPC <rudopisovnick@gmail.com>
2022-04-04 15:05:10 +0400

    Initial commit

---
+++ src/test/java/com/rudnick/yourbox/YourBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rudnick.yourbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
